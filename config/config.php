c2f4376eff16d5e44a5ba4f22cd020b1520e2413
henry <henry@wsleft.lan>
2016-10-30 13:50:33 +0100

    init

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * GeoShipping extension for Isotope eCommerce provides an shipping-method that calculates the shippingprice based on kilometers between shop-postalcode and shipping-postalcode
+ *
+ * Copyright (c) 2016 Henry Lamorski
+ *
+ * @package GeoShipping
+ * @author  Henry Lamorski <henry.lamorski@mailbox.org>
+ */
+
+
+/**
+ * Attributes
+ */
+\Isotope\Model\Payment::registerModelType('PayPalFloat', 'Isotope\Model\Payment\PayPalFloat');
