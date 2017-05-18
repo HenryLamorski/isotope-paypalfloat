243f11fbe4003a9e6e69c85a8f8a474a63d4394e
Henry Lamorski <henry.lamorski@mailbox.org>
2017-05-18 13:05:57 +0200

    add missing DCA config for blacklist

--- contao/dca/tl_iso_payment.php
+++ contao/dca/tl_iso_payment.php
@@ -9,7 +9,7 @@
  */
 $GLOBALS['TL_DCA']['tl_iso_payment']['palettes']['PayPalFloat'] = str_replace(
     '{config_legend},new_order_status',
-    '{config_legend},uos_paypalfloat,new_order_status',
+    '{config_legend},uos_paypalfloat,uos_paypalfloat_blacklist,new_order_status',
     $GLOBALS['TL_DCA']['tl_iso_payment']['palettes']['paypal']
 );
 
@@ -24,4 +24,13 @@ $GLOBALS['TL_DCA']['tl_iso_payment']['fields']['uos_paypalfloat'] = array
     'options_callback'          => array('Isotope\Model\Payment\PayPalFloat', 'getDcaOptions'),
     'eval'                      => array('tl_class'=>'clr'),
     'sql'                       => 'varchar(255)',
+);
+
+$GLOBALS['TL_DCA']['tl_iso_payment']['fields']['uos_paypalfloat_blacklist'] = array
+(
+    'label'                     => array('Blacklist','Blacklist attributes for Productname in Paypal-cart.'),
+    'inputType'                 => 'select',
+    'options_callback'          => array('Isotope\Model\Payment\PayPalFloat', 'getDcaOptions'),
+    'eval'                      => array('multiple'=>true,'size'=>5),
+    'sql'                       => 'blob',
 );
